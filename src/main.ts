a26439bc9ffe2f6081ab4ce37a1953276e43b31d
Andrii Yusyp <a.yusyp@apiko.com>
2023-07-16 22:58:57 +0300

    update port to default

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const port = process.env.PORT || 3000;
+  const port = 3000;
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug', 'log', 'verbose'],
   });
